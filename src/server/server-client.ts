dea0013e02607c3b77802a71864afc9fb0bd9dff
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-06-15 01:12:17 +0300

    ref: Change server framework from exprss to fastify

--- src/server/server-client.ts
+++ src/server/server-client.ts
@@ -1,75 +1,55 @@
-import Express, { Application, Request, Response } from 'express'
-import { App } from '../app'
-import { RoutesConst } from './constants'
+import fastify, { FastifyRequest, FastifyReply } from "fastify";
+import { App } from "../app";
+import { RoutesConst } from "./constants";
 
-type RunningServer = ReturnType<Application['listen']>
+//type RunningServer = ReturnType<Application["listen"]>;
 
 export class AuthServer {
-  public app: App
+  public app: App;
 
-  private express = Express()
-  private runningServer?: RunningServer
-  private serverStarted?: Date
+  private server = fastify();
+  private serverStarted?: Date;
 
-  private twitchCodeReceived?: Date
-  private spotifyCodeReceived?: Date
+  private twitchCodeReceived?: Date;
+  private spotifyCodeReceived?: Date;
 
-  private autoCloseHtml = `<html><script>window.close()</script></html>`
+  private autoCloseHtml = `<html><script>window.close()</script></html>`;
 
   constructor(app: App) {
-    this.app = app
+    this.app = app;
 
-    this.express.get(RoutesConst.SPOTI_CODE, this.handleSpotifyCode.bind(this))
-    this.express.get(RoutesConst.TWITCH_CODE, this.handleTwitchCode.bind(this))
+    this.server.get(RoutesConst.SPOTI_CODE, this.handleSpotifyCode.bind(this));
+    this.server.get(RoutesConst.TWITCH_CODE, this.handleTwitchCode.bind(this));
   }
 
   public startServer() {
-    if (!this.runningServer) {
-      this.serverStarted = new Date()
-      this.runningServer = this.express.listen(process.env.AUTH_SERVER_PORT)
+    if (!this.serverStarted) {
+      this.serverStarted = new Date();
+      this.server.listen({
+        port: Number(process.env.AUTH_SERVER_PORT),
+      });
     }
-
-    setTimeout(() => {
-      if (
-        this.serverStarted &&
-        (!this.twitchCodeReceived || !this.spotifyCodeReceived)
-      ) {
-        this.killServer()
-        process.exit(0)
-      }
-
-      if (
-        this.serverStarted &&
-        (this.twitchCodeReceived || this.spotifyCodeReceived)
-      ) {
-        this.killServer()
-      }
-
-      this.serverStarted = undefined
-      this.twitchCodeReceived = undefined
-      this.spotifyCodeReceived = undefined
-    }, 5 * 1000 * 60)
   }
 
-  private killServer() {
-    if (!this.runningServer) return
+  private handleSpotifyCode(req: FastifyRequest, res: FastifyReply) {
+    this.spotifyCodeReceived = new Date();
 
-    this.runningServer.close()
+    res.send(this.autoCloseHtml);
+    const query = req.query as {
+      code: string;
+    };
+
+    this.app.spotifyClient.authService.authWithCode(query.code);
   }
 
-  private handleSpotifyCode(req: Request, res: Response) {
-    this.spotifyCodeReceived = new Date()
+  private handleTwitchCode(req: FastifyRequest, res: FastifyReply) {
+    this.twitchCodeReceived = new Date();
 
-    res.send(this.autoCloseHtml)
+    res.send(this.autoCloseHtml);
+    const query = req.query as {
+      code: string;
+    };
 
-    this.app.spotifyClient.authService.authWithCode(`${req.query.code}`)
-  }
-
-  private handleTwitchCode(req: Request, res: Response) {
-    this.twitchCodeReceived = new Date()
-
-    res.send(this.autoCloseHtml)
-
-    this.app.twitchClient.getTokensFromCode(`${req.query.code}`)
+    this.app.twitchClient.getTokensFromCode(query.code);
   }
 }
